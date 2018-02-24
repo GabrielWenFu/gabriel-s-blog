1ba3a472212b2a29f2f3260c0268c9b71161547b
guowenfu <guowenfu@heartsone.net>
2018-02-24 15:29:34 +0000

    gabriel's blod

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>111111</div>
+
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style scoped lang='stylus'>
+  @import '~common/stylus/reset.styl'
+  div
+    color: #202020
+    font-size: 14px
+</style>
